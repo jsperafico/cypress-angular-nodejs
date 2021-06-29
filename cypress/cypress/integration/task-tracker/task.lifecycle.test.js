907de3638b0cbdb7e5f1c1419d5691a0587cdd8c
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2021-06-29 14:00:41 +0200

    different getElement sample

--- cypress/cypress/integration/task-tracker/task.lifecycle.test.js
+++ cypress/cypress/integration/task-tracker/task.lifecycle.test.js
@@ -11,16 +11,17 @@ describe('Task lifecycle validateion', () => {
 
     it('Should: add, edit and remove', () => {
         const taskId = Math.random() * 100;
+        const inputed = `This is my random task: ${taskId}`;
 
-        cy.get('input[id=text]').type(`This is my random task: ${taskId}`);
+        cy.get('input[id=text]').type(inputed);
         cy.get('input[id=day]').type(`${Math.ceil(Math.random() * 21)}/${Math.ceil(Math.random() * 12)}/${(Math.ceil(Math.random() * 10)) + 2000}`);
         cy.get('input[type=submit]').click();
 
-        cy.get('app-task-item h3').contains(`${taskId}`);
+        cy.get('app-task-item').contains('h3', inputed).should('be.visible');
         cy.get('app-task-item h3').contains(`${taskId}`).dblclick();
-        cy.get('app-task-item div.reminder h3').contains(`${taskId}`);
+        cy.get('app-task-item div.reminder').contains('h3', inputed).should('be.visible');
 
-        cy.get('app-task-item h3').contains(`${taskId}`).find('fa-icon').click();
+        cy.get('app-task-item').contains('h3', inputed).find('fa-icon').click();
         cy.get('app-task-item h3').should('not.contain', `${taskId}`);
     })
 });
